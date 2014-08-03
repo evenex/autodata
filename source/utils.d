2748e2b5e9c2bf57dc09acf96739636bd7d8a8bc
evenex <vlevenfeld@gmail.com>
2014-08-03 01:14:15 -0400

    notes

--- source/utils.d
+++ source/utils.d
@@ -1,22 +1,22 @@
 module evx.utils;
 
 // REVIEW & REFACTOR
-import std.traits;
-import std.typecons;
-import std.typetuple;
-import std.container;
-import std.algorithm;
-import std.math;
-import std.range;
-import std.datetime;
-import std.string;
-import std.ascii;
-import std.concurrency;
-import std.conv;
-import resource.arrays: is_dynamic_array;
+//import std.traits;
+//import std.typecons;
+//import std.typetuple;
+//import std.container;
+//import std.algorithm;
+//import std.math;
+//import std.range;
+//import std.datetime;
+//import std.string;
+//import std.ascii;
+//import std.concurrency;
+//import std.conv;
+//import resource.arrays: is_dynamic_array;
 
-import evx.meta;
-import evx.functional: zip, map;
+//import evx.meta;
+//import evx.functional: zip, map;
 
 debug = profiler;
 
@@ -27,9 +27,10 @@ public {/*misc}*/
 		{/*...}*/
 			return !value;
 		}
+		
 	const bool not (alias predicate, Args...)(const Args args)
 		if (isSomeFunction!predicate)
-		{/*...}*/
+		{/*...}*/;9
 			return not (predicate (args));
 		}
 
@@ -58,7 +59,7 @@ public {/*misc}*/
 	*/
 	enum imp;
 }
-public {/*debug}*/
+debug {/*}*/
 	/* warning exception */
 	class Warning: Exception
 		{/*...}*/
@@ -67,14 +68,16 @@ public {/*debug}*/
 					super (message, file, line, next);
 				}
 		}
+		
 	/* print detailed information about a caught exception */
 	deprecated auto print_stack_trace (Ex, T...) (Ex exception, T message)
 		if (is (Ex: Throwable))
 		{/*...}*/
-			import std.stdio;
 			const string type = is (Ex == Warning)? `warning`: `error`;
-			stderr.writeln ("tid("~tid_string~"): "~type~" ", message," @"~exception.file~"(", exception.line, "): "~exception.msg);
-			if (!is (Ex == Warning))
+
+			stderr.writeln (`tid(` ~tid_string~ `): ` ~type~ ` `, message, ` @` ~exception.file~ `(`, exception.line, `): ` ~exception.msg);
+
+			if (not (is (Ex == Warning)))
 				{/*...}*/
 					stderr.writeln ("----------------");
 					foreach (frame; exception.info)
@@ -82,127 +85,113 @@ public {/*debug}*/
 				}
 			stderr.flush;
 		}
+		
 	/* output the names and current values of the fields a given record or pointer target record */
 	template examine_fields (alias record, bool show_types = false, T = typeof (record))
 		{/*...}*/
-			import std.stdio;
 			void examine_fields ()
 				{/*...}*/
 					static if (isAggregateType!T)
 						alias Record = T;
 					else static if (isPointer!T)
 						alias Record = PointerTarget!T;
-					else static assert (0, "can't examine fields of "~T.stringof);
+					else static assert (0, `can't examine fields of ` ~T.stringof);
 
-					string record_type = Record.stringof~' ';
-					writeln (record_type~__traits (identifier, record)~`:`);
+					string record_type = Record.stringof~ ' ';
+					writeln (record_type ~__traits (identifier, record)~ `:`);
 
 					foreach (field; __traits (allMembers, Record))
 						{/*...}*/
 							const bool is_field = field != `this` && __traits (compiles, typeof (__traits (getMember, record, field)));
+							
 							static if (not (is_field))
 								continue;
-							else
-								{/*...}*/
-									alias field_T = typeof (__traits (getMember, record, field));
-									
-									static if (isPointer!field_T)
-										alias Field = PointerTarget!field_T;
-									else alias Field = field_T;
+							else {/*...}*/
+								alias field_T = typeof (__traits (getMember, record, field));
+								
+								static if (isPointer!field_T)
+									alias Field = PointerTarget!field_T;
+								else alias Field = field_T;
 
-									static if (show_types)
-										string type = Field.stringof~' ';
-									else 
-										string type = "";
+								static if (show_types)
+									string type = Field.stringof~' '; // TODO tilde formatting
+								else 
+									string type = "";
 
-									alias member = Alias!(__traits (getMember, record, field));
-									static if (__traits (getProtection, member) == `public`)
-										{/*...}*/
-											static if (isSomeFunction!member)
-												{/*...}*/
-													static if (functionAttributes!member & FunctionAttribute.property) 
-														const bool okay_to_print = true;
-													else const bool okay_to_print = false;
-												}
-											else const bool okay_to_print = true;
-										}
-									else const bool okay_to_print = false;
+								alias member = Alias!(__traits (getMember, record, field));
+								static if (__traits (getProtection, member) == `public`) // TODO is_accesible
+									{/*...}*/
+										static if (isSomeFunction!member)
+											{/*...}*/
+												static if (functionAttributes!member & FunctionAttribute.property) 
+													const bool okay_to_print = true;
+												else const bool okay_to_print = false;
+											}
+										else const bool okay_to_print = true;
+									}
+								else const bool okay_to_print = false;
 
-									static if (okay_to_print)
-										writeln (` `~type~field~`: `, __traits (getMember, record, field));
-								}
+								static if (okay_to_print)
+									writeln (` ` ~type~field~ `: `, __traits (getMember, record, field));
+							}
 						}
 					writeln (`---`);
 				}
 		}
+		
 	/* print a function call with arguments */
 	const string function_call_to_string (string name, Args...) (Args args)
 		{/*...}*/
 			string call;
+			
 			foreach (arg; args)
-				call ~= to!string (arg) ~ ", ";
+				call ~= arg.text~ `, `;
+				
 			if (call.length == 0)
-				return name~" ()";
-			else return name~" ("~call[0..$-2]~")";
+				return name;
+			else return name~ ` (` ~call[0..$-2]~ `)`;
 		}
+		
 	/* suppress stderr while in scope */
-	template error_suppression ()
+	template error_suppression (int line = __LINE__)
 		{/*...}*/
 			const string error_suppression ()
 				{/*...}*/
-					import std.string;
-					const string S = __TIMESTAMP__.removechars (":").removechars (" ").succ.translate (['A':'q', 'o':'7', '1':'y', '4':'f', 'J':'4', 'T':'X', 'S':'5', 'e':'_']);
-					const string ID = (S[2*$/3..$].succ~S[$/5..$/3].succ~S[$/2..3*$/4].succ~S[1..$/4].succ).succ;
-					return `
-						int errstream`~ID~`;
-						{/*...}*/
+					const string ID = __LINE__.text;
+		
+					return q{
+						int errstream} ~ID~ q{;
+						}`{`q{
 							import std.c.stdio;
 							import std.c.linux.linux;
-							errstream`~ID~` = dup (stderr.fileno ());
+							
+							errstream} ~ID~ q{ = dup (stderr.fileno);
 							freopen ("/dev/null", "w", stderr);
-						}
-						scope (exit) {/*...}*/
+						}`}`q{
+						scope (exit) }`{`q{
 							import std.c.stdio;
 							import std.c.linux.linux;
+							
 							fflush (stderr);
-							dup2 (errstream`~ID~`, stderr.fileno ());
-						}`
-					;
+							dup2 (errstream} ~ID~ q{, stderr.fileno);
+						}`}`q{
+					};
 				}
 		}
-	/* report the status of a unit test */
-	deprecated template report_test (string name)
-		{/*...}*/
-			const string report_test = `
-				{
-					import std.stdio;
-					stderr.writeln ("initiating `~name~` test...");
-					stderr.flush;
-				}
-				scope (success) {
-					import std.stdio;
-					stderr.writeln ("`~name~` test passed");
-					stderr.flush;
-				}
-				scope (failure) {
-					import std.stdio;
-					stderr.writeln ("`~name~` test failed!");
-					stderr.flush;
-				}
-			`;
-		}
+		
 	/* write information about the thread environment to the output */
 	/* and time program execution through checkpoints */
 	struct Profiler
 		{/*...}*/
-			private import std.datetime;
+			enum ExitStatus {success, failure}
+			
 			public:
-				enum ExitStatus {success, failure}
 			public {/*interface}*/
 				static Profiler begin (string func_name = __PRETTY_FUNCTION__)
 					{/*...}*/
 						return Profiler (func_name);
 					}
+					
 				void end (ExitStatus exit_status)
 					{/*...}*/
 						debug (profiler)
@@ -211,13 +200,15 @@ public {/*debug}*/
 								this.exit_status = exit_status;
 							}
 					}
+					
 				void checkpoint (T...)(T marks)
 					{/*...}*/
 						debug (profiler)
 							{/*...}*/
-								import std.stdio;
 								auto check = Clock.currTime - last_check;
+								
 								this.last_check = Clock.currTime;
+								
 								stderr.writeln (check, ` `, marks);
 								stderr.flush;
 							}
@@ -226,12 +217,12 @@ public {/*debug}*/
 			public {/*~}*/
 				~this ()
 					{/*...}*/
-						debug (profiler)
+						debug (profiler) // REVIEW do we need debug
 							{/*...}*/
 								import std.stdio;
 								stderr.writeln (profiler_exit_indent,
 									exit_status == ExitStatus.failure? `FAILED!`:``, 
-									`ex(`~tid_string~`): `, func_name, ` after `, exit_time-entry_time);
+									`ex(` ~tid_string~`): `, func_name, ` after `, exit_time-entry_time);
 								stderr.flush;
 							}
 					}
@@ -253,7 +244,7 @@ public {/*debug}*/
 								this.func_name = func_name;
 								this.entry_time = Clock.currTime;
 								this.last_check = entry_time;
-								stderr.writeln (profiler_enter_indent, `in(`~tid_string~`): `, func_name, ` at `, entry_time.toISOExtString["2014-05-15".length..$]);
+								stderr.writeln (profiler_enter_indent, `in(` ~tid_string~ `): `, func_name, ` at `, entry_time.toISOExtString["2014-05-15".length..$]);
 							}
 					}
 				@disable this ();
@@ -262,9 +253,9 @@ public {/*debug}*/
 	const string profiler (string name = q{profiler})()
 		{/*...}*/
 			return q{
-				auto }~name~q{ = Profiler.begin;
-				scope (success) }~name~q{.end (Profiler.ExitStatus.success);
-				scope (failure) }~name~q{.end (Profiler.ExitStatus.failure);
+				auto } ~name~ q{ = Profiler.begin;
+				scope (success) } ~name~ q{.end (Profiler.ExitStatus.success);
+				scope (failure) } ~name~ q{.end (Profiler.ExitStatus.failure);
 			};
 		}
 	public {/*formatting}*/
@@ -281,7 +272,7 @@ public {/*debug}*/
 		static uint indent = 0;
 	}
 }
-pure nothrow {/*algorithm}*/
+public {/*move}*/
 	/* move all elements in a range (starting at index) up by one position */
 	/* leaving an empty space at the indexed position */
 	void shift_up_from (R)(ref R range, size_t index)
@@ -294,6 +285,7 @@ pure nothrow {/*algorithm}*/
 			for (size_t i = range.length-1; i > index; --i)
 				range[i] = range[i-1];
 		}
+		
 	/* move all elements in a range (starting at index) down one position */
 	/* overwriting the element at the indexed position */
 	void shift_down_on (R)(ref R range, size_t index)
@@ -310,47 +302,24 @@ pure nothrow {/*algorithm}*/
 public {/*containers}*/
 	template PriorityQueue (T)
 		{/*...}*/
-			import std.container: BinaryHeap;
 			alias PriorityQueue = BinaryHeap!(Array!(T), "a > b"); // REVIEW
 		}
 }
 public {/*multithreading}*/
 	/* get the thread id of the current thread as a string truncated to some digits */
-	const string tid_string (bool owner = false, uint digits = 4) (Tid to_translate = Tid.init) // REVIEW
+	const string tid_string (bool owner = false, uint digits = 4)(Tid to_translate = Tid.init) // REVIEW
 		{/*...}*/
-			import std.conv;
 			static if (owner)
 				auto tid = ownerTid;
 			else auto tid = thisTid;
+			
 			if (to_translate != Tid.init)
 				tid = to_translate;
-			return to!string (*cast(ulong*) &tid) [$-digits..$];
-		}
-	/* actively clear a threads messagebox for a set time, */
-	/* in debug mode, also print the message */
-	void flush_messages (Duration watch_time = 1.msecs)
-		in {/*...}*/
-			assert (watch_time >= 1.msecs);
-		}
-		body {/*...}*/
-			import std.concurrency;
-			import std.datetime;
-			import std.stdio;
-			auto clock = Clock.currTime;
-			while (Clock.currTime < clock + watch_time)
-				{/*...}*/
-					receiveTimeout (clock + watch_time - Clock.currTime, (Variant _)
-						{/*...}*/
-							debug {/*}*/
-								stderr.writeln ("tid("~tid_string~"): warning: cleared unread "~to!string(_.type)~" message"); 
-								stderr.flush;
-							}
-						});
-				}
+				
+			return (*cast(ulong*) &tid).text [$-digits..$];
 		}
 }
-public {/*tuples}*/
-	template λ (alias f) {alias λ = f;}
+public {/*tuple}*/
 	template Aⁿ (T...) {alias Aⁿ = T;}
 }
 public {/*ranges}*/
@@ -359,7 +328,8 @@ public {/*ranges}*/
 	struct Contigious (R)
 		if (allSatify!(And!(is_indexable, Not!isForwardRange), R, ElementType!R))
 		{/*...}*/
-			private size_t i, j;
+			// TODO pure nothrow
+			private size_t i, j; // TODO private
 			R ranges;
 
 			this (R ranges)
@@ -368,7 +338,6 @@ public {/*ranges}*/
 				}
 			@property auto length ()
 				{/*...}*/
-					import math: sum;
 					return ranges.map!(r => r.length).sum;
 				}
 
@@ -396,7 +365,8 @@ public {/*ranges}*/
 	struct Contigious (R)
 		if (allSatisfy!(isForwardRange, R, ElementType!R))
 		{/*...}*/
-			R ranges;
+			// TODO pure nothrow
+			R ranges; // TODO private
 
 			this (R ranges)
 				{/*...}*/
@@ -433,22 +403,28 @@ public {/*ranges}*/
 			return Contigious!R (ranges);
 		}
 	unittest {/*contigious}*/
+		import std.range: equal;
+	
 		int[2] x = [1,2];
 		int[2] y = [3,4];
 		int[2] z = [5,6];
 
 		int[2][] A = [x,y,z];
 
-		assert (A.contigious.sum == 21);
+		assert (A.contigious.equal ([1,2,3,4,5,6]));
 	}
 
 	/* traverse a range with elements rotated left by some number of positions 
 	*/
 	auto rotate_elements (R)(R range, long positions = 1)
-		{/*...}*/
+		in {/*...}*/
+			auto n = range.length;
+			assert ((positions + n) % n > 0);
+		}
+		body {/*...}*/
 			auto n = range.length;
 			auto i = (positions + n) % n;
-			assert (i > 0);
+			
 			return range.cycle[i..n+i];
 		}
 
@@ -461,13 +437,13 @@ public {/*ranges}*/
 
 	/* test if an attempted slice will be within some bounds 
 	*/
-	bool slice_within_bounds (size_t i, size_t j, size_t length) pure nothrow
+	bool slice_within_bounds (size_t i, size_t j, size_t length)
 		{/*...}*/
 			return i <= j && j <= length && i < length;
 		}
 }
 public {/*indices}*/
-	import evx.math: Interval;
+// TODO	import evx.math: Interval;
 	alias Index = size_t;
 	alias Indices = Interval!Index;
 }
