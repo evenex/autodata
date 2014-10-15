7b8542a26320e894eab82bd04434d9979ccbcd06
evenex <vlad@localhost.localdomain>
2014-10-15 02:08:43 -0400

    subbuffers and buffers optimized

--- source/buffer_interface.d
+++ source/buffer_interface.d
@@ -5,20 +5,22 @@ import std.conv;
 
 struct BufferInfo (T)
 	{/*...}*/
-		enum has_element = is(T.ElementType);
+		// mandatory
+		enum has_element = 	is(T.ElementType);
 
-		enum can_access = __traits(compiles, T.init.access (0) = T.ElementType.init);
+		enum can_access = 	__traits(compiles, T.init.access (0) = T.ElementType.init);
+		enum can_resize = 	__traits(compiles, T.init.resize (0) == true);
+		enum can_nullify = 	__traits(compiles, T.init.nullify);
+		enum can_copy = 	__traits(compiles, T.init.copy_from (T.ElementType[].init, 0, 0));
 
-		enum can_resize = __traits(compiles, T.init.resize (0) == true);
-		enum can_nullify = __traits(compiles, T.init.nullify);
+		enum supports_buffer_interface = has_element && can_access && can_resize && can_nullify && can_copy;
 
-		enum can_passive_copy = __traits(compiles, T.init.copy_from (T.ElementType[].init, 0, 0));
-		enum can_active_copy = __traits(compiles, T.init.copy_to (T.ElementType[].init, 0, 0));
-
-		enum supports_buffer_interface = has_element && can_access && can_resize && can_nullify && can_passive_copy;
+		// optional
+		enum can_active_copy = 	__traits(compiles, T.init.copy_to (T.ElementType[].init, 0, 0));
+		enum can_passive_copy =	__traits(compiles, T.init.ptr);
 
 	}
-string show_ct_info (T)()
+string show_ct_info (T)() // REFACTOR to meta
 	{/*...}*/
 		string report = T.stringof~ `:`"\n";
 
@@ -67,9 +69,7 @@ mixin template BufferInterface ()
 				{/*...}*/
 					this = null;
 
-					if (this.resize (range.length))
-						this._length = range.length;
-					else assert (0, `failed to resize ` ~typeof(this).stringof~ ` to ` ~length.text);
+					set_length (range.length);
 
 					this[] = range;
 				}
@@ -93,7 +93,7 @@ mixin template BufferInterface ()
 				}
 			auto opIndex (Slice slice)
 				{/*...}*/
-					return Sub (&sub_access, [slice[0], slice[1]]);
+					return Sub (&this, [slice[0], slice[1]]);
 				}
 
 			auto opIndexAssign (R)(R range)
@@ -106,7 +106,7 @@ mixin template BufferInterface ()
 					auto j = slice[1];
 
 					static if (BufferInfo!R.can_active_copy)
-						range.copy_to (this, i, j);
+						range.copy_to (this[i..j]);
 					else this.copy_from (range, i, j);
 				}
 
@@ -116,8 +116,9 @@ mixin template BufferInterface ()
 				}
 			auto opIndexAssign (ElementType element, Slice slice)
 				{/*...}*/
-					foreach (i; slice[0]..slice[1])
-						this.access (i) = element;
+					import std.range: repeat;
+
+					this.copy_from (element.repeat (slice.interval.length), slice[0], slice[1]);
 				}
 		}
 		public {/*slicing}*/
@@ -133,13 +134,13 @@ mixin template BufferInterface ()
 				{/*...}*/
 					public:
 					public {/*range}*/
-						auto ref front ()
+						ref ElementType front ()
 							{/*...}*/
-								return *access (bounds[0]);
+								return this[0];
 							}
-						auto ref back ()
+						ref ElementType back ()
 							{/*...}*/
-								return *access (bounds[1] - 1);
+								return this[$-1];
 							}
 
 						auto popFront ()
@@ -176,7 +177,7 @@ mixin template BufferInterface ()
 					public {/*opIndex*}*/
 						auto ref opIndex (size_t i)
 							{/*...}*/
-								return *access (bounds[0] + i);
+								return buffer.access (bounds[0] + i);
 							}
 
 						auto opIndex ()
@@ -188,7 +189,7 @@ mixin template BufferInterface ()
 								assert ((slice.vector + bounds[0]).array.interval.is_contained_in (this.bounds.interval));
 							}
 							body {/*...}*/
-								return Sub (access, [bounds[0] + slice[0], bounds[0] + slice[1]]);
+								return Sub (buffer, (bounds[0] + slice.vector).array);
 							}
 
 						auto opIndexAssign (R)(R range)
@@ -197,30 +198,20 @@ mixin template BufferInterface ()
 							}
 						auto opIndexAssign (R)(R range, Slice slice)
 							{/*...}*/
-								import evx.traits: is_indexable;
+								auto subrange = opIndex (slice).bounds;
 
-								auto subrange = opIndex (slice);
-
-								foreach (i; subrange.bounds[0]..subrange.bounds[1])
-									static if (is_indexable!R)
-										*access (i) = range[i - subrange.bounds[0]];
-									else {/*...}*/
-										*access (i) = range.front;
-										range.popFront;
-									}
+								(*buffer)[subrange[0]..subrange[1]] = range;
 							}
 
 						auto opIndexAssign (ElementType element, size_t i)
 							{/*...}*/
-								*access (bounds[0] + i) = element;
+								(*buffer)[bounds[0] + i] = element;
 							}
 						auto opIndexAssign (ElementType element, Slice slice)
 							{/*...}*/
-								auto subrange = opIndex (slice);
+								auto subrange = opIndex (slice).bounds;
 
-								foreach (i; subrange.bounds[0]..subrange.bounds[1])
-									*access (i) = element;
-									// sub can pack a ptr to buf and a set of functions that cast and call... saves space.
+								(*buffer)[subrange[0]..subrange[1]] = element;
 							}
 					}
 					public {/*slicing}*/
@@ -231,7 +222,7 @@ mixin template BufferInterface ()
 					}
 					private:
 					private {/*data}*/
-						ElementType* delegate(size_t) access;
+						This* buffer;
 						size_t[2] bounds;
 					}
 				}
@@ -239,21 +230,16 @@ mixin template BufferInterface ()
 		public {/*appending}*/
 			auto opOpAssign (string op: `~`)(ElementType element)
 				{/*...}*/
-					if (not (this.resize (length + 1)))
-						assert (0, `failed to resize ` ~This.stringof);
-
-					++_length;
+					set_length (length + 1);
 
 					this[$-1] = element;
 				}
+
 			auto opOpAssign (string op: `~`, R)(R range)
 				{/*...}*/
 					auto start = length;
 
-					if (not (this.resize (length + range.length)))
-						assert (0, `failed to resize ` ~This.stringof);
-
-					_length = start + range.length;
+					set_length (length + range.length);
 
 					this[start..$] = range;
 				}
@@ -261,140 +247,146 @@ mixin template BufferInterface ()
 		public {/*popping}*/
 			auto pop (size_t count = 1)
 				{/*...}*/
-					_length -= count;
+					set_length (length - count);
 				}
 		}
 		private:
-		private {/*sub access}*/
-			auto sub_access (size_t i)
-				{/*...}*/
-					return &access (i);
-				}
-		}
 		private {/*data}*/
 			size_t _length;
+
+			auto set_length (size_t new_length)
+				{/*...}*/
+					if (this.resize (new_length))
+						this._length = new_length;
+					else assert (0, `failed to resize ` ~This.stringof~ ` to ` ~new_length.text);
+				}
 		}
 	}
 
 void main ()
 	{/*...}*/
-		struct Buffer
-			{/*...}*/
-				alias ElementType = int;
+		{/*single buffer demo}*/
+			struct Buffer
+				{/*...}*/
+					alias ElementType = int;
 
-				int[666] array;
+					int[666] array;
 
-				public {/*array subinterface}*/
-					ref access (size_t i)
-						{/*...}*/
-							return array[i];
-						}
-					auto resize (size_t i)
-						{/*...}*/
-							return i <= 666;
-						}
-					void nullify ()
-						{/*...}*/
-							
-						}
+					public {/*array subinterface}*/
+						ref access (size_t i)
+							{/*...}*/
+								return array[i];
+							}
+						auto resize (size_t i)
+							{/*...}*/
+								return i <= 666;
+							}
+						void nullify ()
+							{/*...}*/
+								
+							}
 
-					void copy_from (R)(R range, size_t i, size_t j)
-						{/*...}*/
-							import std.algorithm: copy;
+						void copy_from (R)(R range, size_t i, size_t j)
+							{/*...}*/
+								import std.algorithm: copy;
 
-							range.copy (array[i..j]);
-						}
+								range.copy (array[i..j]);
+							}
+					}
+
+					mixin BufferInterface;
 				}
 
-				mixin BufferInterface;
-			}
+			Buffer test;
 
-		Buffer test;
+			test = [1,2,3];
 
-		test = [1,2,3];
+			assert (test.length == 3);
 
-		assert (test.length == 3);
+			// static range primitives
+			assert (test.front == 1);
+			assert (test.back == 3);
 
-		// static range primitives
-		assert (test.front == 1);
-		assert (test.back == 3);
+			// element access
+			assert (test[0] == 1);
+			assert (test[1] == 2);
+			assert (test[2] == 3);
+			assert (test[$-1] == 3);
 
-		// element access
-		assert (test[0] == 1);
-		assert (test[1] == 2);
-		assert (test[2] == 3);
-		assert (test[$-1] == 3);
+			// slice equality
+			assert (test[0..1] == [1]);
+			assert (test[1..3] == [2,3]);
+			assert (test[0..$] == [1,2,3]);
 
-		// slice equality
-		assert (test[0..1] == [1]);
-		assert (test[1..3] == [2,3]);
-		assert (test[0..$] == [1,2,3]);
+			// slice equivalence
+			assert (test[] == [1,2,3]);
+			assert (test[].front == test.front);
+			assert (test[].back == test.back);
+			assert (test[][0] == test[0]);
+			assert (test[][1] == test[1]);
+			assert (test[][2] == test[2]);
+			assert (test[][0..1] == test[0..1]);
+			assert (test[][1..3] == test[1..3]);
 
-		// slice equivalence
-		assert (test[] == [1,2,3]);
-		assert (test[].front == test.front);
-		assert (test[].back == test.back);
-		assert (test[][0] == test[0]);
-		assert (test[][1] == test[1]);
-		assert (test[][2] == test[2]);
-		assert (test[][0..1] == test[0..1]);
-		assert (test[][1..3] == test[1..3]);
+			// element mutation
+			test[0] = 4;
+			assert (test[0] == 4);
+			assert (test[] == [4,2,3]);
 
-		// element mutation
-		test[0] = 4;
-		assert (test[0] == 4);
-		assert (test[] == [4,2,3]);
+			// range mutation
+			test[] = [4,5,6];
+			assert (test[] == [4,5,6]);
 
-		// range mutation
-		test[] = [4,5,6];
-		assert (test[] == [4,5,6]);
+			// subrange mutation
+			test[0..2] = [7,8];
+			assert (test[] == [7,8,6]);
 
-		// subrange mutation
-		test[0..2] = [7,8];
-		assert (test[] == [7,8,6]);
+			// slice mutation
+			auto slice = test[1..3];
+			assert (slice[] == [8,6]);
+			slice[] = [9,9];
+			assert (slice[] == [9,9]);
+			slice[0] = 8;
+			assert (test[] == [7,8,9]);
+			slice[0..2] = 6;
+			assert (test[] == [7,6,6]);
 
-		// slice mutation
-		auto slice = test[1..3];
-		assert (slice[] == [8,6]);
-		slice[] = [9,9];
-		assert (slice[] == [9,9]);
-		slice[0] = 8;
-		assert (test[] == [7,8,9]);
-		slice[0..2] = 6;
-		assert (test[] == [7,6,6]);
+			// slice range traversal
+			test[] = [1,1,1];
+			foreach (x; test[])
+				assert (x == 1);
+			foreach (x; test[0..2])
+				assert (x == 1);
 
-		// slice range traversal
-		test[] = [1,1,1];
-		foreach (x; test[])
-			assert (x == 1);
-		foreach (x; test[0..2])
-			assert (x == 1);
+			// reassignment
+			test = [9,0];
+			assert (test.length == 2);
 
-		// reassignment
-		test = [9,0];
-		assert (test.length == 2);
+			// element appending
+			test ~= 1;
+			assert (test.length == 3);
+			assert (test[] == [9,0,1]);
 
-		// element appending
-		test ~= 1;
-		assert (test.length == 3);
-		assert (test[] == [9,0,1]);
+			// range appending
+			test ~= [2,3];
+			assert (test.length == 5);
+			assert (test[] == [9,0,1,2,3]);
 
-		// range appending
-		test ~= [2,3];
-		assert (test.length == 5);
-		assert (test[] == [9,0,1,2,3]);
+			// popping
+			test.pop;
+			assert (test.length == 4);
+			assert (test[] == [9,0,1,2]);
+			test.pop (2);
+			assert (test.length == 2);
+			assert (test[] == [9,0]);
 
-		// popping
-		test.pop;
-		assert (test.length == 4);
-		assert (test[] == [9,0,1,2]);
-		test.pop (2);
-		assert (test.length == 2);
-		assert (test[] == [9,0]);
-
-		// nullification
-		test = null;
-		assert (test.length == 0);
-		assert (test[].empty);
-		assert (test.empty);
+			// nullification
+			test = null;
+			assert (test.length == 0);
+			assert (test[].empty);
+			assert (test.empty);
+		}
+		{/*active/passive copying}*/
+			
+		}
 	}
