94fd9f844334dc699b60c40db6cf3950e19173eb
evenex <vlad@localhost.localdomain>
2014-12-14 07:36:04 -0500

    no local/marray pending space impl

--- source/evx/containers/package.d
+++ source/evx/containers/package.d
@@ -1,5 +1,3 @@
 module evx.containers;
 public:
 import evx.containers.set;
-import evx.containers.m_array;
-import evx.containers.local_view;
--- source/evx/containers/set.d
+++ source/evx/containers/set.d
@@ -18,6 +18,10 @@ struct Set (T)
 				foreach (item; items)
 					store.remove (item);
 			}
+		auto contains (T item)
+			{/*...}*/
+				return item in store;
+			}
 
 		this (Items...)(Items items)
 			{/*...}*/
@@ -29,4 +33,25 @@ struct Set (T)
 				return store.keys;
 			}
 		mixin IterationOps!elements;
+		mixin SearchOps!contains;
+	}
+	unittest {/*...}*/
+		import evx.math.logic;
+
+		Set!int A;
+
+		A.insert (1);
+		A.insert (3);
+
+		assert (A.contains (1));
+		assert (1 in A);
+		assert (3 in A);
+
+		assert (4 !in A);
+
+		A.remove (1);
+		assert (not (A.contains (1)));
+
+		A.remove (3);
+		assert (3 !in A);
 	}
