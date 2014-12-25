6d4536f1344fc1c7abe65207659759b0e86c48c1
evenex <vlad@localhost.localdomain>
2014-12-24 21:41:29 -0500

    ++grid

---
+++ source/evx/math/grid.d
@@ -0,0 +1,43 @@
+module evx.math.grid;
+
+private {/*imports}*/
+	import std.conv;
+
+	import evx.math.logic;
+	import evx.math.space;
+	import evx.math.intervals;
+
+	import evx.type;
+	import evx.operators;
+	import evx.range;
+	import evx.misc.patch;
+}
+
+struct Grid (Space)
+	{/*...}*/
+		struct Base
+			{/*...}*/
+				Space space;
+				Repeat!(dimensionality!Space, size_t) lengths;
+
+				Element!Space access (typeof(lengths) point)
+					{/*...}*/
+						auto domain_transform (uint d)() {return space.limit!d.left + point[d] * space.limit!d.width / lengths[d];}
+
+						return space[Map!(domain_transform, Count!(typeof(point)))];
+					}
+
+				size_t[2] limit (uint d)() const
+					{/*...}*/
+						return [0, lengths[d]];
+					}
+			}
+
+		mixin Patch!(Base, 13860);
+	}
+auto grid (S, T...)(S space, T cells)
+	{/*...}*/
+		static assert (typeof(cells).length == dimensionality!S);
+
+		return Grid!S (space, cells);
+	}
