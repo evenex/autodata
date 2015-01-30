114ea07992f3788c4fb51c2229aa339572cc60b5
evenex <vlevenfeld@gmail.com>
2015-01-30 17:27:44 -0500

    common interface TODO refactor package

---
+++ source/evx/misc/commoninterface.d
@@ -0,0 +1,62 @@
+module evx.misc.commoninterface; // REFACTOR package
+
+private {/*import}*/
+	import std.conv: text;
+
+	import evx.type;
+	import evx.range;
+}
+
+struct CommonInterface (Types...)
+	if (Types.length > 1)
+	{/*...}*/
+		byte[
+			Reduce!(λ!q{(uint a, uint b) = a > b? a : b},
+				Map!(λ!q{(T) = T.sizeof}, 
+					Types
+				)
+			)
+		] value;
+
+		ref opCast (T)()
+			{/*...}*/
+				return indexed_cast!(IndexOf!(T, Types));
+			}
+
+		auto ref opDispatch (string op, Args...)(Args args)
+			{/*...}*/
+				enum visit (uint i) = q{
+					case } ~ i.text ~ q{:
+						return indexed_cast!} ~ i.text ~ q{.} ~ op ~ q{ (args);
+				};
+
+				switch (current)
+					{/*...}*/
+						mixin([Map!(visit, Count!Types)].join.text);
+						default: assert (0);
+					}
+			}
+
+		auto ref opAssign (T)(auto ref T value)
+			{/*...}*/
+				import evx.misc.memory;
+
+				current = IndexOf!(T, Types);
+
+				this.value[0..T.sizeof] = value.bytes;
+
+				return this;
+			}
+
+		private:
+
+		int current;
+
+		ref indexed_cast (uint i)()
+			in {/*...}*/
+				assert (i == current);
+			}
+			body {/*...}*/
+				return *cast(Types[i]*) value.ptr;
+			}
+	}
