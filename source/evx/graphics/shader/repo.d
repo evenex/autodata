4e2f8e4df8458782697a879752fc8ade14394840
evenex <vlevenfeld@gmail.com>
2015-02-01 07:09:30 -0500

    attempted texture coord scaling fix, bug noted

--- source/evx/graphics/shader/repo.d
+++ source/evx/graphics/shader/repo.d
@@ -2,7 +2,9 @@ module evx.graphics.shader.repo;
 
 private {/*imports}*/
 	import evx.math;
+	import evx.range;
 	import evx.misc.tuple;
+	import evx.misc.memory;
 
 	import evx.graphics.shader;
 	import evx.graphics.texture;
@@ -14,7 +16,11 @@ alias aspect_correction = vertex_shader!(`aspect_ratio`, q{
 
 auto textured_shape_shader (R)(R shape, auto ref Texture texture)
 	{/*...}*/
-		return τ(shape, shape.scale (2).flip!`vertical`).vertex_shader!(
+		static if (is (Element!R == Vector!(2,float)))
+			alias draw_shape = shape;
+		else auto draw_shape = shape.map!(to!(Vector!(2,float)));
+
+		return τ(draw_shape, draw_shape.flip!`vertical`).vertex_shader!( // REVIEW texture coord scaling? definitely a BUG now, proper scaling seems to depend on the shape... thats a big problem
 			`position`, `tex_coords`, q{
 				gl_Position = vec4 (position, 0, 1);
 				frag_tex_coords = (tex_coords + vec2 (1,1))/2;
@@ -24,5 +30,5 @@ auto textured_shape_shader (R)(R shape, auto ref Texture texture)
 			Texture, `tex`, q{
 				gl_FragColor = texture2D (tex, frag_tex_coords);
 			}
-		)(texture);
+		)(forward!texture);
 	}
