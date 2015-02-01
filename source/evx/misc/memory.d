20988eaea285517c267295192393b4bbbbf41879
evenex <vlevenfeld@gmail.com>
2015-02-01 06:07:39 -0500

    attempted ++forwarding, hit some ICE

--- source/evx/misc/memory.d
+++ source/evx/misc/memory.d
@@ -89,6 +89,17 @@ template forward (alias symbol)
 			}
 	}
 
+import evx.type;
+template forward2 (Aliases...) // BUG https://issues.dlang.org/show_bug.cgi?id=14096
+	{/*...}*/
+		auto ref f (uint i)()
+			{/*...}*/
+				return forward!(Aliases[i]);
+			}
+
+		alias forward2 = Map!(f, Count!Aliases);
+	}
+
 /* a borrowed resource bypasses RAII And move semantics 
 */
 struct Borrowed (T)
