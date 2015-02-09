875bb40c7c4a4a84e2f946f262efe8aab06c4d40
evenex <vlevenfeld@gmail.com>
2015-02-09 06:12:35 -0500

    fixed toString undefined reference error

--- source/evx/graphics/text.d
+++ source/evx/graphics/text.d
@@ -24,9 +24,7 @@ private {/*types}*/
 	alias texture_atlas_t = void;
 }
 
-void main () {}
-version (none):
-
+void main (){}
 struct Glyph
 	{/*...}*/
 		immutable(dchar) symbol;
@@ -644,9 +642,9 @@ struct Text
 							return this[].extract!`card`.join.bounding_box;
 						}
 
-					const toString ()
+					auto toString ()
 						{/*...}*/
-							return this[].extract!`symbol`.to!string;
+							return this[].extract!`symbol`.cache.to!string;
 						}
 				}
 
@@ -670,284 +668,23 @@ struct Text
 			BoundingBox draw_box;
 		}
 	}
-static if (0)
-class Text
-	{/*...}*/
-		this (Font font, Display display, string text)
-			{/*...}*/
-				wrapped = Implementation (font, display, text.to!dstring);
-			}
+version (none):
 
-		mixin Wrapped!Implementation;
+void main () {/*...}*/
+	scope gfx = new Display; // BUG need this before using gpu stuff at all... how to enforce?
 
-		mixin View!(wrapped, // REVIEW
-			InvalidateOn!(`within`, `align_to`, `translate`, `rotate`, `scale`),
-			RefreshOn!(`cards`, `colors`, `tex_coords`)
-		);
+	scope f = new Font (12); // class
+	scope x = new Text (f, gfx, `hay sup`);
 
-		mixin TransferOps!(wrapped,
-			ExtendSlice!q{
+	assert (x[][1] == x[1]);
 
-				auto color (Color color)
-					}`{`q{
-						auto me = this;
+	x[].up_from (`s`).color = red;
 
-						foreach (glyph; me)
-							glyph.color = color;
-					}`}`q{
+	assert (x[1] == 'a');
+	x[0..3] = `oi,`;
+	assert (x[1] == 'i');
 
-				auto bounding_box ()
-					}`{`q{
-						return this.extract!`card`.join.bounding_box;
-					}`}`q{
+	assert (x.colors[].stride (4).m_array[] == [black, black, black, black, red, red, red]);
 
-				auto toString ()
-					}`{`q{
-						return this.extract!`symbol`.array.to!string;
-					}`}`q{
-			}
-		);
-
-		struct Implementation
-			{/*...}*/
-				void refresh ()
-					in {/*...}*/
-						assert (font.is_loaded);
-					}
-					body {/*...}*/
-						alias text = data;
-
-						if (text.length == 0)
-							return;
-
-						auto wrap_width = (draw_box.width * î!vec.rotate (rotation)).to_pixel_space (display).norm;
-						auto pen = fvec(0, -font.ascender);
-						this.card_box = [0.fvec, pen].bounding_box; 
-
-						{/*reserve memory}*/
-							cards.clear;
-							tex_coords.clear;
-							newline_positions.clear;
-
-							cards.capacity = 4 * text.length;
-							tex_coords.capacity = 4 * text.length;
-							{/*estimate line breaks}*/
-								auto newlines = text.count!(c => c == '\n');
-								auto approx_glyphs_per_line = (wrap_width / Font.texture_font_get_glyph (font, ' ').width).floor;
-
-								newline_positions.capacity = (newlines + text.length / approx_glyphs_per_line).ceil.to!size_t;
-							}
-						}
-
-						newline_positions ~= 0;
-
-						foreach (i, symbol; text[])
-							{/*build buffers}*/
-								auto glyph = Font.texture_font_get_glyph (font, symbol);
-
-								auto offset = ivec(glyph.offset_x, glyph.offset_y);
-								auto dims = uvec(glyph.width, glyph.height);
-								auto advance = glyph.advance_x;
-
-								with (glyph) 
-								tex_coords ~= [
-									fvec(s0, t0), 
-									fvec(s1, t1)
-								].bounding_box[].flip!`vertical`;
-
-								cards ~= [
-									pen,
-									pen + fvec(dims.x, 0),
-									pen + dims,
-									pen + fvec(0, dims.y)
-								];
-
-								if (pen.x + dims.x > wrap_width)
-									{/*word wrap}*/
-										auto trim_length = text[0..i+1].retro.up_to!isWhite.length;
-
-										if (trim_length < 0)
-											trim_length = i + 1;
-
-										auto cutoff = i + 1 - length;
-											
-										auto word = ℕ[0..trim_length]
-											.map!(j => j + cutoff)
-											.map!(j => cards[4*j..4*(j+1)]);
-
-										auto Δx = (word.empty? 
-											pen.x + advance : word[0][0].x
-										) - offset.x;
-
-										auto carriage_return (fvec v) {return v - fvec(Δx, font.height);} 
-
-										foreach (ref letter; word)
-											letter.transform!(map!carriage_return);
-
-										pen = carriage_return (pen);
-
-										newline_positions ~= i - word.length + 1;
-									}
-								else if (symbol == '\n')
-									{/*line break}*/
-										newline_positions ~= i;
-
-										pen = fvec(0, pen.y - font.height);
-									}
-
-								cards[$-4..$] = cards[$-4..$].map!(v => v - fvec(-offset.x, dims.y - offset.y));
-
-								pen.x += advance;
-
-								card_box.width = max (card_box.width, pen.x);
-							}
-
-						{/*align text}*/
-							card_box.height = max (pen.y.abs, font.height);
-							card_box = card_box.align_to (Alignment.top_left, 0.fvec);
-
-							newline_positions ~= data.length;
-
-							foreach (i, line_start; enumerate (newline_positions[0..$-1]))
-								{/*justify lines}*/
-									auto line_stop = newline_positions[i+1];
-
-									if (line_stop == line_start)
-										continue;
-
-									auto line_box = cards[4*line_start..4*line_stop].bounding_box;
-
-									auto justification = line_box.offset_to (alignment, card_box).x;
-
-									auto line = cards[4*line_start..4*line_stop];
-
-									line[] = line.map!(v => v + fvec(justification, 0));
-								}
-
-							auto transform (fvec v) {return scale * (v-pen/2).rotate (rotation) + pen/2;}
-
-							card_box = card_box[].map!transform.bounding_box;
-
-							cards[] = cards[].map!transform
-								.map!(v => v + card_box.offset_to (alignment, draw_box))
-								.map!(v => v.to_normalized_space (display) + translation)
-								.map!(v => v.each!(to!float));
-						}
-					}
-
-				auto texture ()
-					{/*...}*/
-						return font.texture;
-					}
-
-				this (ref Font font, Display display, dstring text)
-					{/*...}*/
-						this.font = font;
-						this.display = display;
-						this.draw_box = display.normalized_bounds; 
-						this.data = text;
-
-						{/*reserve memory}*/
-							colors.clear;
-							colors.capacity = 4*text.length;
-						}
-
-						colors ~= black.repeat (4*text.length);
-
-						refresh;
-					}
-
-				mixin AffineTransform!float;
-
-				public:
-				public {/*alignment}*/
-					ref align_to (Alignment alignment)
-						{/*...}*/
-							this._alignment = alignment;
-
-							return this;
-						}
-					auto alignment ()
-						{/*...}*/
-							return _alignment;
-						}
-
-					ref within (BoundingBox box)
-						{/*...}*/
-							this.draw_box = box;
-
-							return this;
-						}
-					auto bounding_box ()
-						{/*...}*/
-							return draw_box;
-						}
-				}
-				public {/*transfer}*/
-					auto access (size_t i)
-						{/*...}*/
-							return Glyph (data[i], colors[4*i..4*(i+1)], cards[4*i..4*(i+1)]);
-						}
-					auto pull (R)(R range, size_t i, size_t j)
-						{/*...}*/
-							static if (is_string!R)
-								{/*...}*/
-									data = data[0..i] ~ range.to!dstring ~ data[j..$];
-
-									refresh;
-								}
-							else {/*...}*/
-								foreach (k, glyph; enumerate (range))
-									colors[4*k..4*(k+1)] = glyph.color;
-
-								auto old_data = data;
-
-								data = data[0..i] ~ range.map!(glyph => glyph.symbol).to!dstring ~ data[j..$];
-
-								if (data != old_data)
-									refresh;
-							}
-						}
-					auto length () const
-						{/*...}*/
-							return data.length;
-						}
-				}
-				public {/*...}*/
-					Appendable!VertexBuffer cards;
-					Appendable!VertexBuffer tex_coords;
-					Appendable!ColorBuffer colors;
-				}
-				private:
-				private {/*data}*/
-					Font font;
-
-					dstring data;
-					Appendable!(MArray!size_t) newline_positions;
-
-					Display display; // REVIEW need this
-					Alignment _alignment;
-					Box!float card_box; // REVIEW out here why?
-					BoundingBox draw_box;
-				}
-			}
-	}
-static if (0)
-	unittest {/*...}*/
-		scope gfx = new Display; // BUG need this before using gpu stuff at all... how to enforce?
-
-		scope f = new Font (12); // class
-		scope x = new Text (f, gfx, `hay sup`);
-
-		assert (x[][1] == x[1]);
-
-		x[].up_from (`s`).color = red;
-
-		assert (x[1] == 'a');
-		x[0..3] = `oi,`;
-		assert (x[1] == 'i');
-
-		assert (x.colors[].stride (4).m_array[] == [black, black, black, black, red, red, red]);
-
-		assert (x[] == `oi, sup`);
-	}
+	assert (x[] == `oi, sup`);
+}
