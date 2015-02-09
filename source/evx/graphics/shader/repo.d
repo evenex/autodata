204588dc0b1e1cc197825268f4b3b1b9a1da9287
evenex <vlevenfeld@gmail.com>
2015-02-09 03:08:01 -0500

    automatic texture forwarding

--- source/evx/graphics/shader/repo.d
+++ source/evx/graphics/shader/repo.d
@@ -2,6 +2,7 @@ module evx.graphics.shader.repo;
 
 private {/*imports}*/
 	import evx.math;
+	import evx.type;
 	import evx.range;
 	import evx.memory;
 	import evx.misc.tuple;
@@ -20,16 +21,22 @@ auto textured_shape_shader (R, T)(R shape, auto ref T texture)
 			alias draw_shape = shape;
 		else auto draw_shape = shape.map!(to!(Vector!(2,float)));
 
-		return τ(draw_shape, draw_shape.flip!`vertical`.into_bounding_box_of (square (2.0f)))
-			.vertex_shader!(
-				`position`, `tex_coords`, q{
-					gl_Position = vec4 (position, 0, 1);
-					frag_tex_coords = (tex_coords + vec2 (1,1))/2;
-				}
-			).fragment_shader!(
-				fvec, `frag_tex_coords`,
-				Texture, `tex`, q{
-					gl_FragColor = texture2D (tex, frag_tex_coords);
-				}
-			)(texture);
+		static if (__traits(isRef, texture) && is (InitialType!T == T))
+			auto draw_texture = borrow (texture);
+		else alias draw_texture = texture;
+
+		return τ(
+			draw_shape,
+			draw_shape.flip!`vertical`.into_bounding_box_of (square (2.0f))
+		).vertex_shader!(
+			`position`, `tex_coords`, q{
+				gl_Position = vec4 (position, 0, 1);
+				frag_tex_coords = (tex_coords + vec2 (1,1))/2;
+			}
+		).fragment_shader!(
+			fvec, `frag_tex_coords`,
+			Texture, `tex`, q{
+				gl_FragColor = texture2D (tex, frag_tex_coords);
+			}
+		)(draw_texture);
 	}
