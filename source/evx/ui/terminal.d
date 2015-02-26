0057e00aac02d7227f413ff65c9dd37c3e6ee4ce
evenex <vlevenfeld@gmail.com>
2015-02-25 21:18:27 -0500

    working but messy command map

--- source/evx/ui/terminal.d
+++ source/evx/ui/terminal.d
@@ -9,8 +9,23 @@ private {/*import}*/
 	import evx.containers;
 }
 
+struct AppendFilter(alias filter, T) // REVIEW
+	{/*...}*/
+		T core;
+		alias core this;
+
+		auto ref opOpAssign (string op : `~`, Args...)(Args args)
+			{/*...}*/
+				if (filter (args))
+					core ~= args;
+			}
+	}
+
 struct Terminal
 	{/*...}*/
+		bool is_open;
+		CommandMap command_map;
+
 		Display display;
 		Input input;
 
@@ -19,7 +34,12 @@ struct Terminal
 		Color console_color;
 		Text console_text;
 
-		Stack!(dchar[], OnOverflow.reallocate) text_buffer; // TODO filter out ~` 
+		Queue!(Array!dchar, OnOverflow.reallocate)
+			command_history; // REVIEW only recoverable in O(n), maybe this isn't a big deal
+
+		AppendFilter!(x => x.not!contained_in ("~`"),
+			Stack!(Array!dchar, OnOverflow.reallocate)
+		) text_buffer;
 
 		this (size_t width, size_t height)
 			{/*...}*/
@@ -39,8 +59,13 @@ struct Terminal
 				input.bind (Input.Key.tilde, hit
 					=> hit? (
 						input.is_streaming_text?
-							 input.text_stream (null) : input.text_stream (text_buffer) // BUG gets evaluated in the wrong order : if true, RHS, else LHS
+							 input.text_stream (null)
+							 : input.text_stream (text_buffer) // BUG gets evaluated in the wrong order : if true, RHS, else LHS
 					) : {}
+				);
+				input.bind (Input.Key.enter, hit
+					=> hit && input.is_streaming_text?
+						apply_current_command () : {}
 				);
 
 				auto geometry = [fvec(-1, 1), fvec(1, 1), fvec(1, 0.5), fvec(-1, 0.5)];
@@ -51,7 +76,11 @@ struct Terminal
 				console_color = green (0.2);
 
 				console_text = Text (Font (12), display);
-				console_text.within (console_border_geometry[].cache.scale (scale*scale).bounding_box); // REVIEW sucks that we have to manually convert float -> double
+				console_text.within (console_border_geometry[].cache.scale (scale*scale).bounding_box) // REVIEW sucks that we have to manually convert float -> double
+					.align_to (Alignment.bottom_left);
+
+				is_open = true;
+				command_map[`quit`] = (·){is_open = false;};
 			}
 
 		void show_console () // TODO drop-down animation
@@ -64,7 +93,7 @@ struct Terminal
 					.basic_shader (console_color * white)
 					.line_loop.render_to (display);
 
-				console_text = text_buffer[];
+				console_text = chain (command_history[], text_buffer[]);
 				console_text[].color = console_color (1.0);
 				console_text.render_to (display);
 			}
@@ -77,7 +106,29 @@ struct Terminal
 				display.post;
 				input.process;
 			}
+
+		void apply_current_command ()
+			{/*...}*/
+				if (text_buffer[].empty)
+					return;
+
+				if (auto command = text_buffer[].split (` `).front.cache.to!string in command_map)
+					(*command)([]);
+
+				command_history ~= text_buffer[];
+				command_history ~= "\n";
+
+	//			command_history -=  TODO cull stuff past a certain amount
+
+				text_buffer.clear;
+
+				// HACK BUG enter key gets stuck for some reason
+				input.text_stream (null);
+				input.text_stream (text_buffer);
+			}
 	}
+
+alias CommandMap = void delegate(string[])[string];
 
 void main ()
 	{/*...}*/
@@ -85,7 +136,7 @@ void main ()
 
 		auto terminal = Terminal (512, 512);
 
-		foreach (_; 0..300)
+		while (terminal.is_open)
 			{/*...}*/
 				terminal.refresh;
 
