8c21e83462cc33e71caeb211dbd27a6cdd35a05b
evenex <vlevenfeld@gmail.com>
2015-02-25 21:17:21 -0500

    swap buffers

--- source/evx/async/adaptors/package.d
+++ source/evx/async/adaptors/package.d
@@ -3,5 +3,5 @@ module evx.async.adaptors;
 public:
 import evx.async.adaptors.mutex;
 import evx.async.adaptors.future;
-//import evx.async.adaptors.swap; TODO
+import evx.async.adaptors.swap;
 //import evx.async.adaptors.roster; TODO
