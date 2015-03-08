11ca53721baa6e76cfbc89ab5e9b547d65f5a9b1
evenex <vlevenfeld@gmail.com>
2015-03-08 05:50:37 -0400

    added delegate stream

---
+++ source/evx/experimental/stream.d
@@ -0,0 +1,48 @@
+import std.functional;
+import evx.operators;
+import evx.range;
+import evx.type;
+import evx.math;
+
+// sampling frequency turns continuous index to discrete index
+// interpolation turns continuous index to value over discrete indices
+/*	
+	;
+*/
+struct Stream (Sample, Bounds)
+	{/*...}*/
+		alias Index = Element!Bounds;
+
+		Sample delegate(Index) source;
+		Bounds delegate() bounds;
+
+		const measure ()
+			{/*...}*/
+				return bounds ();
+			}
+
+		mixin SliceOps!(source, measure);
+	}
+auto stream_from (Source, Bounds)(Source source, Bounds bounds)
+	{/*...}*/
+		return Stream!(ReturnType!source, ReturnType!bounds)(source.toDelegate, bounds.toDelegate);
+	}
+
+auto s (T, uint n)(T[n] array)
+	{/*...}*/
+		return array;
+	}
+
+void main ()
+	{/*...}*/
+		auto f = (int x) => double (x).sqrt;
+		auto b = () => [0, 100].s;
+
+		auto stream = stream_from (f,b);
+
+		import std.stdio;
+
+		writeln (stream[0..12]);
+
+		stdout.flush;
+	}
