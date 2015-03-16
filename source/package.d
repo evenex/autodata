df08babc4e4a5c5c827b84179c7b0d600c5a5baa
evenex <vlevenfeld@gmail.com>
2015-03-16 17:37:15 -0400

    added main spacecadet package

---
+++ source/package.d
@@ -0,0 +1,12 @@
+module spacecadet;
+
+public:
+import spacecadet.core;
+import spacecadet.functional;
+import spacecadet.memory;
+import spacecadet.meta;
+import spacecadet.operators;
+import spacecadet.patterns;
+import spacecadet.sequence;
+import spacecadet.spaces;
+import spacecadet.topology;
