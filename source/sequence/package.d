d55a2c4d6f21fe18cd907ff022c33b51f2dc93b5
evenex <vlevenfeld@gmail.com>
2015-03-18 03:29:34 -0400

    added sequence adaptors

--- source/sequence/package.d
+++ source/sequence/package.d
@@ -2,6 +2,7 @@ module spacecadet.sequence;
 
 public:
 
+import spacecadet.sequence.adaptors;
 import spacecadet.sequence.arithmetic;
 import spacecadet.sequence.iteration;
 import spacecadet.sequence.numerical;
