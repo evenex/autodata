49cf3200bd2693daded6b9b8b7e7257239ce71a9
evenex <vlevenfeld@gmail.com>
2015-05-31 20:41:31 -0400

    added lexicographic traversal

--- source/autodata/traversal.d
+++ source/autodata/traversal.d
@@ -1,6 +1,10 @@
 module autodata.traversal;
 
 private {//import
+	import evx.meta;
+	import evx.interval;
+
+	import autodata.traits;
 	import autodata.functional;
 	import autodata.spaces.sequence;
 }
@@ -13,4 +17,62 @@ auto enumerate (R)(R range)
 if (is_input_range!R && has_length!R)
 {
 	return zip (Nat[0..range.length], range);
+}
+
+/* lexicographic traversal
+*/
+struct Lexicographic (S)
+{
+	S space;
+
+	Repeat!(dimensionality!S, size_t) index;
+
+	auto front ()
+	{
+		auto idx (uint i)()
+		{
+			return index[i] + space.limit!i.left;
+		}
+
+		return space[Map!(idx, Ordinal!index)];
+	}
+	auto popFront ()
+	{
+		void advance (uint i = 0)()
+		{
+			if (++index[i] >= space.limit!i.width)
+				static if (i+1 < index.length)
+				{
+					index[i] = 0;
+					advance!(i+1);
+				}
+		}
+
+		advance;
+	}
+	auto empty ()
+	{
+		return index[$-1] >= space.limit!(index.length-1).width;
+	}
+
+	auto opEquals (R)(R range)
+	{
+		import std.algorithm: equal;
+
+		return this.equal (range);
+	}
+}
+auto lexicographic (S)(S space)
+{
+	return Lexicographic!S (space);
+}
+alias lexi = lexicographic;
+unittest {
+	import autodata.spaces;
+
+	auto test = Array!(int, 2)(
+		[1,2,3].extrude (Nat[0..3])
+	);
+
+	assert (test.lexi == [1,2,3,1,2,3,1,2,3]);
 }
